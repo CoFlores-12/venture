bc07d1cc9750ac3978cd8f7e53a94bfdb0b927c5
Daniel Avila <daniel.avila.a.v.2000@gmail.com>
2025-07-06 15:52:31 -0600

    Avance en el backend de compras

--- app/api/purchase/route.js
+++ app/api/purchase/route.js
@@ -1,41 +1,70 @@
 import { NextResponse } from 'next/server';
 import Purchase from '@/src/models/purchase';
+import Event from '@/src/models/event';
 import { connectToMongoose } from '@/src/lib/db';
 
+//registra una nueva nueva venta
 export async function POST(req) {
   await connectToMongoose();
 
   try {
     const body = await req.json();
-    const { user, event, ticketQuantity, totalAmount } = body;
+    const { user, event, ticketQuantity, typeTicket} = body;
 
-    if (!user || !event || !ticketQuantity || !totalAmount) {
+    if (!user || !event || !ticketQuantity || !typeTicket ) {
       return NextResponse.json({ error: 'Missing fields' }, { status: 400 });
     }
 
-    const newPurchase = await Purchase.create({
-      user,
-      event,
-      ticketQuantity,
-      totalAmount,
-    });
+    const eventFind = await Event.findById(event);
+    if (!eventFind) {
+      return NextResponse.json({ error: "Evento no encontrado" }, { status: 404 });
+    }
 
-    return NextResponse.json(newPurchase, { status: 201 });
+    const selectedTicket = eventFind.tickets.find(ticket => ticket.name === typeTicket);
 
-  } catch (error) {
-    console.error('Purchase creation error:', error);
-    return NextResponse.json({ error: 'Server error' }, { status: 500 });
-  }
+    if (!selectedTicket) {
+      return NextResponse.json({ error: "Tipo de boleto no válido" }, { status: 400 });
+    }
+
+    if (selectedTicket.quantityAvailable < ticketQuantity) {
+      return NextResponse.json({ error: "No hay suficientes boletos disponibles" }, { status: 400 });
+    }
+
+    const totalAmount = selectedTicket.price * ticketQuantity;
+
+    selectedTicket.quantityAvailable -= ticketQuantity;
+
+    await eventFind.save();
+
+        const newPurchase = await Purchase.create({
+          user,
+          event,
+          ticketQuantity,
+          totalAmount,
+          typeTicket
+        });
+
+        return NextResponse.json(newPurchase, { status: 201 });
+
+      } catch (error) {
+        console.error('Purchase creation error:', error);
+        return NextResponse.json({ error: 'Server error' }, { status: 500 });
+      }
 }
 
-
+// trea todos las compras
 export async function GET() {
   try {
     await connectToMongoose();
 
     const purchases = await Purchase.find()
-      // falta hacer los join con el evento y el usuario de la compra
-      .sort({ createdAt: -1 }); 
+      // falta hacer los join con el usuario de la compra
+      .populate({
+        path: "event",
+        select: "title date",
+      })
+      .sort({ createdAt: -1 })
+      .lean();
 
     return NextResponse.json(purchases, { status: 200 });
 
