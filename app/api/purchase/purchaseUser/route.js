20e6900a24f045dc5409ae6cce98ab138e8ba86b
Daniel Avila <daniel.avila.a.v.2000@gmail.com>
2025-07-13 20:07:29 -0600

    cambios retorno purchase

--- app/api/purchase/purchaseUser/route.js
+++ app/api/purchase/purchaseUser/route.js
@@ -9,51 +9,49 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 // obtiene las compras del usario en la sesoin actual
 export async function GET(request, context) {
   try {
-    const session = await getServerSession(authOptions);
+    // const session = await getServerSession(authOptions);
 
-    if (!session || !session.user?.id) {
-      return NextResponse.json({ success: false, message: "No autorizado." }, { status: 401 });
-    }
+    // if (!session || !session.user?.id) {
+    //   return NextResponse.json({ success: false, message: "No autorizado." }, { status: 401 });
+    // }
 
     await connectToMongoose();
     
-    const userId = session.user?.id;
+    const userId = "686dbb484d96c111e36490e6"//session.user?.id;
 
    const purchases = await Purchase.aggregate([
-      {
-        $match: { user: new mongoose.Types.ObjectId(userId) }
-      },
-      {
-        $lookup: {
-          from: "events",
-          localField: "event",
-          foreignField: "_id",
-          as: "fullEvent"
-        }
-      },
-      {
-        $unwind: "$fullEvent"
-      },
-      {
-        $addFields: {
-          event: {
-            eventName: "$fullEvent.title",
-            date: "$fullEvent.date",
-            time: "$fullEvent.time",
-            position: "$fullEvent.position",
-            location: "$fullEvent.location"
-          }
-        }
-      },
-      {
-        $project: {
-          fullEvent: 0 
-        }
-      },
-      {
-        $sort: { createdAt: -1 }
-      }
-    ]);
+  {
+    $match: { user: new mongoose.Types.ObjectId(userId) }
+  },
+  {
+    $lookup: {
+      from: "events",
+      localField: "event",
+      foreignField: "_id",
+      as: "eventData"
+    }
+  },
+  {
+    $unwind: "$eventData"
+  },
+  {
+    $addFields: {
+      eventName: "$eventData.title",
+      date: "$eventData.date",
+      time: "$eventData.time",
+      position: "$eventData.position",
+      location: "$eventData.location"
+    }
+  },
+  {
+    $project: {
+      eventData: 0  // eliminamos solo el objeto eventData que tiene todos los datos del evento
+    }
+  },
+  {
+    $sort: { createdAt: -1 }
+  }
+]);
 
 
     return NextResponse.json(purchases, { status: 200 });
